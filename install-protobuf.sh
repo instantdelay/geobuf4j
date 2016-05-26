0294983342fc43c3be7e5f307389423a5eea0327
Spencer Van Hoose <spencer@instantdelay.com>
2016-05-25 22:50:57 -0700

    Attempt to fix travis - install protoc before build

---
+++ install-protobuf.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Install protoc on travis-ci
+# Thanks to https://github.com/travis-ci/container-example/blob/master/install-protobuf.sh
+set -e
+# check to see if protobuf folder is empty
+if [ ! -d "$HOME/protobuf/lib" ]; then
+  wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz
+  tar -xzvf protobuf-2.6.1.tar.gz
+  cd protobuf-2.4.1 && ./configure --prefix=$HOME/protobuf && make && make install
+else
+  echo "Using cached directory."
+fi
