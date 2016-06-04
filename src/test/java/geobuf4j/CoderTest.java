eb5cd8836fb8bb547262e24d83014b4e9c46b51b
Spencer Van Hoose <spencer@instantdelay.com>
2016-06-04 14:02:52 -0700

    Simplify encode/decode test by extracting test geometries to file

--- src/test/java/geobuf4j/CoderTest.java
+++ src/test/java/geobuf4j/CoderTest.java
@@ -3,6 +3,9 @@ package geobuf4j;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
 import org.junit.Test;
 
 import com.instantdelay.geobuf4j.GeometryDecoder;
@@ -16,52 +19,19 @@ import com.vividsolutions.jts.io.WKTReader;
 public class CoderTest {
 
     private static final int precision = 6;
-    
+
     private GeometryFactory gf = new GeometryFactory(new PrecisionModel(Math.pow(10, precision)));
     private WKTReader wkt = new WKTReader(gf);
     private GeometryEncoder encoder = new GeometryEncoder(precision, false);
     private GeometryDecoder decoder = new GeometryDecoder(precision, false);
 
     @Test
-    public void roundTripPoint() throws ParseException {
-        Geometry p = wkt.read("POINT (10 10.11111111)");
-        assertThat(decoder.decode(encoder.encode(p)), is(p));
-    }
-
-    @Test
-    public void roundTripMultiPoint() throws ParseException {
-        Geometry g = wkt.read("MULTIPOINT(0 0, 5 5, 25 3, -81 3)");
-        assertThat(decoder.decode(encoder.encode(g)), is(g));
-        g = wkt.read("MULTIPOINT(0 0, 0.0001 0, 0.0001 0.0001, 0 0.0001)");
-        assertThat(decoder.decode(encoder.encode(g)), is(g));
-    }
-
-    @Test
-    public void roundTripLineString() throws ParseException {
-        Geometry g = wkt.read("LINESTRING(0 0, 5 5, 25 3, -81 3)");
-        assertThat(decoder.decode(encoder.encode(g)), is(g));
-    }
-
-    @Test
-    public void roundTripMultiLineString() throws ParseException {
-        Geometry g = wkt.read("MULTILINESTRING((0 0, 5 5, 25 3, -81 3), (0 0, 5 5, 25 3, -81 3), (-1 -1, -1 44, 0 0, 100 3.554444))");
-        assertThat(decoder.decode(encoder.encode(g)), is(g));
-    }
-
-    @Test
-    public void roundTripPolygon() throws ParseException {
-        Geometry g = wkt.read("POLYGON((0 0, 5 0, 5 5, 0 5, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1))");
-        assertThat(decoder.decode(encoder.encode(g)), is(g));
-    }
-
-    @Test
-    public void roundTripMultiPolygon() throws ParseException {
-        Geometry g = wkt.read("MULTIPOLYGON("
-                + " ((0 0, 5 0, 5 5, 0 5, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1)), "
-                + " ((0 0, 5 0, 5 5, 0 5, 0 0), (1 1, 1 2, 2 2, 2 1, 1 1)), "
-                + " ((0 0, 1 0, 1 1, 0 0))"
-                + ")");
-        assertThat(decoder.decode(encoder.encode(g)), is(g));
+    public void encodeAndDecodeProducesOriginal() throws FileNotFoundException, ParseException {
+        InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream("/geometries.wkt"));
+        Geometry g;
+        while ((g = wkt.read(reader)) != null) {
+            assertThat(decoder.decode(encoder.encode(g)), is(g));
+        }
     }
 
 }
